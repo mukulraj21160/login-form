231d3bdd5b81edbf086d60bdd1c14f7377156351
Mukul Raj <mukulraj21160@gmail.com>
2021-04-28 13:05:36 +0530

    completes-glassmorphism-form

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,65 +1,259 @@
 import React, { useState } from "react";
 import { Button, Grid, makeStyles } from "@material-ui/core";
-// import backgroundimg from "../Assets/pencils.png";
+import backgroundimg from "../Assets/car.jpg";
 
 const useStyle = makeStyles((theme) => ({
   form_container: {
-    fontFamily: "Sacramento, cursive",
-    minHeight: "100vh",
-    width: "100vw",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "space-evenly",
-    // backgroundImage: "url(" + backgroundimg + ")",
-    backgroundColor: "#6807f9",
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-    perspective: "1000px",
+    fontFamily: theme.typography.body1.fontFamily,
+    [theme.breakpoints.up("xs")]: {
+      fontFamily: "Sacramento, cursive",
+      minHeight: "100vh",
+      width: "100vw",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "space-evenly",
+      backgroundImage: "url(" + backgroundimg + ")",
+      // backgroundColor: "#6807f9",
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      perspective: "1000px",
+    },
+    [theme.breakpoints.up("sm")]: {
+      fontFamily: "Sacramento, cursive",
+      minHeight: "100vh",
+      width: "100vw",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "space-evenly",
+      backgroundImage: "url(" + backgroundimg + ")",
+      // backgroundColor: "#6807f9",
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      perspective: "1000px",
+    },
+
+    [theme.breakpoints.up("md")]: {
+      fontFamily: "Sacramento, cursive",
+      minHeight: "100vh",
+      width: "100vw",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "space-evenly",
+      backgroundImage: "url(" + backgroundimg + ")",
+      // backgroundColor: "#6807f9",
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      perspective: "1000px",
+    },
+    [theme.breakpoints.up("lg")]: {
+      fontFamily: "Sacramento, cursive",
+      minHeight: "100vh",
+      width: "100vw",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "space-evenly",
+      backgroundImage: "url(" + backgroundimg + ")",
+      // backgroundColor: "#6807f9",
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      perspective: "1000px",
+    },
   },
   form: {
-    height: "430px",
-    width: "350px",
-    background: "#fab95b",
-    textAlign: "center",
-    alignItems: "center",
-    borderRadius: "5px",
-    boxShadow: "0 5px 15px rgba (0,0,0,.3) ",
-    borderTop: "1px solid rgba (255,255,255,.3) ",
-    borderLeft: "1px solid rgba (255,255,255,.3) ",
-    backdropFilter: "blur (10px) ",
-    transformStyle: "preserve-3d",
+    fontFamily: theme.typography.body1.fontFamily,
+    [theme.breakpoints.up("xs")]: {
+      height: "430px",
+      width: "350px",
+      background: "rgba(255,255,255,.1)",
+      textAlign: "center",
+      alignItems: "center",
+      borderRadius: "5px",
+      boxShadow: "0 5px 15px rgba (0,0,0,.3) ",
+      borderTop: "1px solid rgba (255,255,255,.3) ",
+      borderLeft: "1px solid rgba (255,255,255,.3) ",
+      backdropFilter: "blur (10px) ",
+      transformStyle: "preserve-3d",
+    },
+    [theme.breakpoints.up("sm")]: {
+      height: "430px",
+      width: "350px",
+      background: "rgba(255,255,255,.1)",
+      textAlign: "center",
+      alignItems: "center",
+      borderRadius: "5px",
+      boxShadow: "0 5px 15px rgba (0,0,0,.3) ",
+      borderTop: "1px solid rgba (255,255,255,.3) ",
+      borderLeft: "1px solid rgba (255,255,255,.3) ",
+      backdropFilter: "blur (10px) ",
+      transformStyle: "preserve-3d",
+    },
+
+    [theme.breakpoints.up("md")]: {
+      height: "430px",
+      width: "350px",
+      background: "rgba(255,255,255,.1)",
+      textAlign: "center",
+      alignItems: "center",
+      borderRadius: "5px",
+      boxShadow: "0 5px 15px rgba (0,0,0,.3) ",
+      borderTop: "1px solid rgba (255,255,255,.3) ",
+      borderLeft: "1px solid rgba (255,255,255,.3) ",
+      backdropFilter: "blur (10px) ",
+      transformStyle: "preserve-3d",
+    },
+    [theme.breakpoints.up("lg")]: {
+      height: "430px",
+      width: "350px",
+      background: "rgba(255,255,255,.1)",
+      textAlign: "center",
+      alignItems: "center",
+      borderRadius: "5px",
+      boxShadow: "0 5px 15px rgba (0,0,0,.3) ",
+      borderTop: "1px solid rgba (255,255,255,.3) ",
+      borderLeft: "1px solid rgba (255,255,255,.3) ",
+      backdropFilter: "blur (10px) ",
+      transformStyle: "preserve-3d",
+    },
   },
   heading: {
-    fontSize: "30px",
-    color: "#",
-    letterSpacing: "3px",
+    fontFamily: theme.typography.body1.fontFamily,
+    [theme.breakpoints.up("xs")]: {
+      fontSize: "30px",
+      color: "#fff",
+      letterSpacing: "3px",
+    },
+    [theme.breakpoints.up("sm")]: {
+      fontSize: "30px",
+      color: "#fff",
+      letterSpacing: "3px",
+    },
+
+    [theme.breakpoints.up("md")]: {
+      fontSize: "30px",
+      color: "#fff",
+      letterSpacing: "3px",
+    },
+    [theme.breakpoints.up("lg")]: {
+      fontSize: "30px",
+      color: "#fff",
+      letterSpacing: "3px",
+    },
   },
   input_Data: {
-    outline: "none",
-    border: "none",
-    height: "40px",
-    width: "82%",
-    background: "rgba (0,0,0,.1) ",
-    color: "rgba (255,255,255,.7) ",
-    boxShadow: "0 0 5px rgba(0,0,0,.5) inset",
-    fontSize: "17px",
-    padding: "0 10px",
-    margin: "15px 0",
-    letterSpacing: "1px",
-    borderRadius: "15px",
+    fontFamily: theme.typography.body1.fontFamily,
+    [theme.breakpoints.up("xs")]: {
+      outline: "none",
+      border: "none",
+      height: "40px",
+      width: "82%",
+      background: "rgba (0,0,0,.1) ",
+      color: "rgba (255,255,255,.7) ",
+      boxShadow: "0 0 5px rgba(0,0,0,.5) inset",
+      fontSize: "17px",
+      padding: "0 10px",
+      margin: "15px 0",
+      letterSpacing: "1px",
+      borderRadius: "15px",
+    },
+    [theme.breakpoints.up("sm")]: {
+      outline: "none",
+      border: "none",
+      height: "40px",
+      width: "82%",
+      background: "rgba (0,0,0,.1) ",
+      color: "rgba (255,255,255,.7) ",
+      boxShadow: "0 0 5px rgba(0,0,0,.5) inset",
+      fontSize: "17px",
+      padding: "0 10px",
+      margin: "15px 0",
+      letterSpacing: "1px",
+      borderRadius: "15px",
+    },
+
+    [theme.breakpoints.up("md")]: {
+      outline: "none",
+      border: "none",
+      height: "40px",
+      width: "82%",
+      background: "rgba (0,0,0,.1) ",
+      color: "rgba (255,255,255,.7) ",
+      boxShadow: "0 0 5px rgba(0,0,0,.5) inset",
+      fontSize: "17px",
+      padding: "0 10px",
+      margin: "15px 0",
+      letterSpacing: "1px",
+      borderRadius: "15px",
+    },
+    [theme.breakpoints.up("lg")]: {
+      outline: "none",
+      border: "none",
+      height: "40px",
+      width: "82%",
+      background: "rgba (0,0,0,.1) ",
+      color: "rgba (255,255,255,.7) ",
+      boxShadow: "0 0 5px rgba(0,0,0,.5) inset",
+      fontSize: "17px",
+      padding: "0 10px",
+      margin: "15px 0",
+      letterSpacing: "1px",
+      borderRadius: "15px",
+    },
   },
   btn: {
-    fontSize: "20px",
-    fontWeight: "bold",
-    width: "90%",
-    backgroundColor: "#2e3b65",
-    color: "#fff",
-    "&:hover": {
-      color: "#000",
+    fontFamily: theme.typography.body1.fontFamily,
+    [theme.breakpoints.up("xs")]: {
+      fontSize: "20px",
+      fontWeight: "bold",
+      width: "90%",
+      backgroundColor: "#2e3b65",
+      color: "#fff",
+      "&:hover": {
+        color: "#000",
+      },
+      cursor: "pointer",
+      marginTop: "30px",
+      borderRadius: "10px",
     },
-    cursor: "pointer",
-    marginTop: "30px",
-    borderRadius: "10px",
+    [theme.breakpoints.up("sm")]: {
+      fontSize: "20px",
+      fontWeight: "bold",
+      width: "90%",
+      backgroundColor: "#2e3b65",
+      color: "#fff",
+      "&:hover": {
+        color: "#000",
+      },
+      cursor: "pointer",
+      marginTop: "30px",
+      borderRadius: "10px",
+    },
+
+    [theme.breakpoints.up("md")]: {
+      fontSize: "20px",
+      fontWeight: "bold",
+      width: "90%",
+      backgroundColor: "#2e3b65",
+      color: "#fff",
+      "&:hover": {
+        color: "#000",
+      },
+      cursor: "pointer",
+      marginTop: "30px",
+      borderRadius: "10px",
+    },
+    [theme.breakpoints.up("lg")]: {
+      fontSize: "20px",
+      fontWeight: "bold",
+      width: "90%",
+      backgroundColor: "#2e3b65",
+      color: "#fff",
+      "&:hover": {
+        color: "#000",
+      },
+      cursor: "pointer",
+      marginTop: "30px",
+      borderRadius: "10px",
+    },
   },
 }));
 
@@ -87,10 +281,13 @@ const Form = () => {
   return (
     <div className={classes.form_container}>
       <Grid container item>
-        <Grid item lg={4}></Grid>
+        <Grid item lg={4} md={4} sm={1} xs={1}></Grid>
         <Grid
           item
           lg={4}
+          md={4}
+          sm={10}
+          xs={10}
           style={{ display: "flex", justifyContent: "space-evenly" }}
         >
           <form
@@ -128,7 +325,7 @@ const Form = () => {
             </Button>
           </form>
         </Grid>
-        <Grid item lg={4}></Grid>
+        <Grid item lg={4} md={4} sm={1} xs={1}></Grid>
       </Grid>
     </div>
   );
